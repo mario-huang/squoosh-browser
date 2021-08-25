6d014863ab1e0992fe2283768949d9f68264e274
Mario <myeveryheart@qq.com>
2021-08-25 15:27:57 +0800

    save

--- src/features/encoders/browserJPEG/client/index.ts
+++ src/features/encoders/browserJPEG/client/index.ts
@@ -1,6 +1,5 @@
 import { canvasEncode } from '../../../../client/lazy-app/util/canvas';
 import WorkerBridge from '../../../../client/lazy-app/worker-bridge';
-import { qualityOption } from '../../../../features/client-utils';
 import { mimeType, EncodeOptions } from '../shared/meta';
 
 export const encode = (
@@ -10,4 +9,3 @@ export const encode = (
   options: EncodeOptions,
 ) => canvasEncode(imageData, mimeType, options.quality);
 
-export const Options = qualityOption({ min: 0, max: 1, step: 0.01 });
